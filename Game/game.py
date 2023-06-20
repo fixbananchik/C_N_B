a93a2f2718d340211f6a51f3e11a0993dfb3f548
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2023-06-20 18:37:24 +0300

    fix

--- Game/game.py
+++ Game/game.py
@@ -1,3 +1,10 @@
 class Game:
     def __init__(self):
-        
+        self.round =
+        self.users = []
+        self.rules = {}
+        self.password = ""
+
+    def start(self):
+
+    def end(self):
